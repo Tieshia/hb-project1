247128eb16e32a047922b9cd76edd65921569963
Tieshia Francis <francistie@gmail.com>
2018-05-08 23:08:01 +0000

    Completed model.py and edited per Instructor suggestions.

--- model.py
+++ model.py
@@ -82,6 +82,7 @@ class StoredIngredient(db.Model):
 
     __tablename__ = "stored_ingredients"
 
+    record_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.ingredient_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     added_at = db.Column(db.DateTime)
@@ -101,19 +102,19 @@ class CookedRecipe(db.Model):
 
     __tablename__ = "cooked_recipes"
 
-    log_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    cooked_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     added_at = db.Column(db.DateTime)
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.recipe_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
 
     recipes = db.relationship('Recipe', backref='cooked_recipes')
-    users = db.Column('User', backref='cooked_recipes')
+    users = db.relationship('User', backref='cooked_recipes')
 
     def __repr__(self):
         """String representation of a recipes cooked."""
 
-        return "<id={} added_at={} recipe_id={} user_id={}>".format(self.log_id,
-            self.added_at self.recipe_id, self.user_id)
+        return "<id={} added_at={} recipe_id={} user_id={}>".format(self.cooked_id,
+            self.added_at, self.recipe_id, self.user_id)
 
 
 class Score(db.Model):
@@ -125,6 +126,7 @@ class Score(db.Model):
     rated_at = db.Column(db.DateTime)
     effort_score = db.Column(db.Integer)
     taste_score = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.recipe_id'))
 
     recipes = db.relationship('Recipe', backref='scores')
@@ -141,11 +143,19 @@ class Score(db.Model):
 ##############################################################################
 # Helper functions
 
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///mealplan'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
@@ -155,6 +165,8 @@ if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    from server import app
-    connect_to_db(app)
-    print "Connected to DB."
+    init_app()
+
+    # from server import app
+    # connect_to_db(app)
+    # print "Connected to DB."
