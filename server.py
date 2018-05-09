d13e18390510081274272c06fa69d16fb787a3ad
Tieshia Francis <francistie@gmail.com>
2018-05-09 00:26:39 +0000

    Create secrets.sh to source Flask secret key for server; created server.py with initial setup; created homepage.html.

---
+++ server.py
@@ -0,0 +1,27 @@
+""" Meal Planning"""
+
+import os
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, request, flash, redirect, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import (connect_to_db, db, User, FoodType, Recipe, Ingredient, 
+    StoredIngredient, CookedRecipe, Score)
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails silently.
+# This is horrible. Fix this so that, instead, it raises an error.
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
