19f5a7a15d0fcf2b2868e4eb4f9d7d02353ada60
Tieshia Francis <francistie@gmail.com>
2018-05-09 00:09:28 +0000

    Created seed.py file for food type table and populated database.

---
+++ seed.py
@@ -0,0 +1,37 @@
+"""Utility file to meal planning database."""
+
+import datetime
+from sqlalchemy import func
+
+from model import (User, FoodType, Recipe, Ingredient, 
+    StoredIngredient, CookedRecipe, Score, connect_to_db, db, init_app)
+
+def load_food_type():
+    """Load food type by list below."""
+
+    food_groups = ['Dairy and Egg Products', 'Spices and Herbs', 'Fats and Oils',
+        'Proteins', 'Soups, Sauces, and Gravies', 'Produce', 'Nuts and Seeds', 
+        'Grains and Pasta']
+
+    for item in food_groups:
+        food_group = FoodType(food_type=item)
+
+        # Add group to the session or it won't ever be stored.
+        db.session.add(food_group)
+
+    # Once done, commit work.
+    db.session.commit()
+
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+if __name__ == "__main__":
+    init_app()
+    db.create_all()
+    load_food_type()
